a0422e9866d027ecdc5523660a3b80500764d274
unknown <yoshiki.yamaguchi2000@gmail.com>
2024-11-20 16:38:33 +0800

    Initial commit

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use(cookieParser()); // Use cookie-parser middleware
 // CORS configuration
 const allowedOrigins = [
     'http://localhost:5173',
-    // 'http://213.190.4.136:5173', // Allow requests from the frontend (production)
-    // 'http://213.190.4.136:5174', // Allow requests from alternate frontend ports (if used)
-    'http://localhost:5000', // Allow requests from local test backend
+    'http://213.190.4.136:5173', // Allow requests from the frontend (production)
+    'http://213.190.4.136:5174', // Allow requests from alternate frontend ports (if used)
+    // 'http://localhost:5000', // Allow requests from local test backend
 ];
 
 
