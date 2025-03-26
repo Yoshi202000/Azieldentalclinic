026a65ca322987d6a112c2f25e942fc8615acd5a
Yoshi202000 <yoshiki.yamaguchi2000@gmail.com>
2025-03-27 04:23:27 +0800

    as

--- src/component/admin/AdminEditSchedule.jsx
+++ src/component/admin/AdminEditSchedule.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
-import '../../test/testSchedule.css';
+import '../../test/TestSchedule.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import Spinner from '../../test/Spinner';
 
